fa2effad3c6330951ab2a5005fa44d8525fc3a8b
Sudarshan Uprety <mr.sudarshanuprety@gmail.com>
2023-03-04 13:05:50 +0545

    created superadmin and categories under admin pannel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin): #This class is used to make sure that the hashed password is not shown in django admin and show other features like is active, date joined etc
+    list_display=('email','first_name','last_name','username','last_login','date_joined','is_active')
+    list_display_links=('email','first_name','last_name')
+    readonly_fields=('last_login','date_joined')
+    ordering=('-date_joined',)
+
+    filter_horizontal=()
+    list_filter=()
+    fieldsets=()
+
+admin.site.register(Account,AccountAdmin)
--- category/admin.py
+++ category/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Category
 
 # Register your models here.
-admin.site.register(Category) #we have imported the model in admin pannel so that the admin can use it
+class CategoryAdmin(admin.ModelAdmin): #here this class is used to make sure that the cateogry name is automatically set to slug name as both of them are same while adding categories
+    prepopulated_fields={'slug':('category_name',)}
+    list_display=('category_name','slug')
+
+admin.site.register(Category,CategoryAdmin) #we have imported the model in admin pannel so that the admin can use it

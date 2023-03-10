d948ab603080e8352bea107b427608957c0950a5
Sudarshan Uprety <mr.sudarshanuprety@gmail.com>
2023-03-10 12:00:07 +0545

    added cart functionality without login feature

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from store.models import Product
 from django.http import HttpResponse
 from .models import Cart,CartItem
-from ..store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 def _cart_id(request): #this function will get the cart id and will use that cart id in add_cart function
@@ -12,16 +13,62 @@ def _cart_id(request): #this function will get the cart id and will use that car
 
 
 def add_cart(request,product_id): #this function is when we click add to cart then this should run
-    product=Product.Object.get(id=product_id) #this function will get the product
+    product=Product.objects.get(id=product_id) #this function will get the product
     try:
-        cart=Cart.Object.get(cart_id=_cart_id(request)) #get the cart using the cart_id present in the session
-    except Cart.DoesnotExist:
-        cart=Cart.Object.create(cart_id=_cart_id(request))
+        cart=Cart.objects.get(cart_id=_cart_id(request)) #get the cart using the cart_id present in the session
+    except Cart.DoesNotExist:
+        cart=Cart.objects.create(cart_id=_cart_id(request))
     cart.save()
 
     try:
-        cart_item=CartItem.objects.get
+        cart_item=CartItem.objects.get(product=product,cart=cart)
+        cart_item.quantity+=1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item=CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart,
+        )
+        cart_item.save()
+    return redirect('cart')
+
+def remove_cart(request,product_id):
+    cart=Cart.objects.get(cart_id=_cart_id(request))
+    product=get_object_or_404(Product,id=product_id)
+    cart_item=CartItem.objects.get(product=product,cart=cart)
+    if cart_item.quantity>1:
+        cart_item.quantity-=1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request,product_id):
+    cart=Cart.objects.get(cart_id=_cart_id(request))
+    product=get_object_or_404(Product,id=product_id)
+    cart_item=CartItem.objects.get(product=product,cart=cart)
+    cart_item.delete()
+    return redirect('cart')
 
 
-def cart(request):
-    return render(request,'store/cart.html')
+def cart(request,total=0,quantity=0,cart_items=None):
+    try:
+        cart=Cart.objects.get(cart_id=_cart_id(request))
+        cart_items=CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total+=(cart_item.product.price*cart_item.quantity)
+            quantity+=cart_item.quantity
+        tax=(0*total)/100 #Tax 0 because in Nepal we do not include the tax as it will have been already included in the product
+        grand_total=total+tax
+        
+    except ObjectDoesNotExist:
+        pass #just ignoring
+    context={
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+        'tax':tax,
+        'grand_total':grand_total,
+    }
+    return render(request,'store/cart.html',context)

a1442eab3c652e35f7fc68b5fe440961d3e57d6a
sudarshan <mr.sudarshanuprety@gmail.com>
2023-03-12 11:43:05 +0545

    added search functionality and other minor fixes

--- store/views.py
+++ store/views.py
@@ -3,6 +3,9 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 
 # Create your views here.
 
@@ -13,13 +16,20 @@ def store(request,category_slug=None):
     if category_slug != None:
         categories=get_object_or_404(Category,slug=category_slug)
         products=Product.objects.filter(category=categories,is_available=True)
+        paginator = Paginator(products,3) #here we are using paginator which is in next button when we click previous next, we have used 6 because we want to show only 6 in one page
+        page=request.GET.get('page')
+        paged_products=paginator.get_page(page)
         product_count=products.count()
     else:
-        products=Product.objects.all().filter(is_available=True)
+        products=Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products,3) #here we are using paginator which is in next button when we click previous next, we have used 6 because we want to show only 6 in one page
+        page=request.GET.get('page')
+        paged_products=paginator.get_page(page)
         product_count=products.count()
 
+
     context={
-        'products':products,
+        'products':paged_products,
         'product_count':product_count,
     }
     return render(request,'store/store.html',context)
@@ -37,3 +47,17 @@ def product_detail(request,category_slug,product_slug):   #this function is used
         'in_cart':in_cart,
     }
     return render(request,'store/product_detail.html',context)
+
+def search(request): #when a search is done this function is called 
+    if 'keyword' in request.GET: #here we are checking if the GET request has option keyword or not
+        keyword=request.GET['keyword'] #if there is keyword then we are storing it into keyword variable
+        if keyword:
+            products = Product.objects.filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword)).order_by('-created_date')
+            product_count=products.count()
+
+        context={
+            'products':products,
+            'product_count':product_count
+        }
+    return render(request,'store/store.html',context) #and this will return the rendered search page
+

91975b2286aee36a8d7a0044ea3afcf9ed97e9e6
sudarshan <mr.sudarshanuprety@gmail.com>
2023-03-18 15:57:22 +0545

    adding user login, create account, authorization and authentication

---
+++ accounts/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import Account
+
+class RegestrationForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter Password',
+        'class':'form-control',
+    }))
+    confirm_password=forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Confirm Password',
+        'class':'form-control',
+    }))
+    
+    class Meta:
+        model=Account
+        fields=['first_name','last_name','phone_number','email','password']
+
+    def __init__(self,*args,**kwarg):
+        super(RegestrationForm,self).__init__(*args,**kwarg)
+        self.fields['first_name'].widget.attrs['placeholder']='Enter Firstname'
+        self.fields['last_name'].widget.attrs['placeholder']='Enter Lastname'
+        self.fields['phone_number'].widget.attrs['placeholder']='Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder']='Enter your email'
+        self.fields['password'].widget.attrs['placeholder']='Enter password'
+        self.fields['last_name'].widget.attrs['placeholder']='Confirm password '
+
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
+
+    def clean(self): #this function will check if the password and confirm password are same or not.
+        cleaned_data=super(RegestrationForm,self).clean()
+        password=cleaned_data.get('password')
+        confirm_password=cleaned_data.get('confirm_password')
+
+        if password!=confirm_password:
+            raise forms.ValidationError(
+                "Password doesnot match!"
+            )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
-
+from .forms import RegestrationForm
+from .models import Account
+from django.contrib import messages,auth
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def register(request):
+    if request.method=='POST':
+        form=RegestrationForm(request.POST)
+        if form.is_valid():
+            first_name=form.cleaned_data['first_name'] #when we use django form we need to use cleaned_data to extract form the frontend
+            last_name=form.cleaned_data['last_name']
+            email=form.cleaned_data['email']
+            phone_number=form.cleaned_data['phone_number']
+            password=form.cleaned_data['password']
+            username=email.split("@")[0]
+
+            user=Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)  #this will call the create_user function which is under accounts/mode.py/MyAccountManager class
+            user.phone_number=phone_number
+            user.save()
+            messages.success(request,'Regestration successful')
+            return redirect('register')
+    else:
+        form=RegestrationForm()
+
+    
+    context={
+        'form':form,
+    }
+    return render(request,'accounts/register.html',context)
+
+
+
+def login(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+
+
+        user=auth.authenticate(email=email,password=password)
+
+
+        if user is not None:
+            auth.login(request,user)
+            #messages.success(request,"you're now logged in")
+            return redirect('home')
+        else:
+            messages.error(request,"Invalid login details")
+            return redirect ('login')
+
+    return render(request,'accounts/login.html')
+
+@login_required(login_url="login")
+def logout(request):
+    auth.logout(request)
+    messages.success(request,"You're logged out.")
+    return redirect('login')

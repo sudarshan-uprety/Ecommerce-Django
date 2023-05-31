92b056d6d869ec72d9c3405ddc5ad823ab48dc09
Sudarshan uprety <mr.sudarshanuprety@gmail.com>
2023-05-31 09:52:22 +0545

    buf fix for remove cart items for non loggedin user

--- carts/views.py
+++ carts/views.py
@@ -172,7 +172,7 @@ def remove_cart_item(request, product_id, cart_item_id):
         cart_item = CartItem.objects.get(product=product, user=request.user, id=cart_item_id)
     
     else:
-        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        cart_item = CartItem.objects.get(product=product, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
 
